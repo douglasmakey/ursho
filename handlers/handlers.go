81724015a0aa7c86a53ebebd2b74de7a79cef08f
Douglasmakey <Douglasmakey@gmail.com>
2017-06-11 01:51:39 -0400

    Use verb handling in handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -15,39 +15,46 @@ type bodyRequest struct {
 
 func EncodeHandler(storage storages.IFStorage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+		if r.Method == http.MethodPost {
 
-		var b bodyRequest
-		if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			e := Error{Data: "Unable to decode JSON request body: " + err.Error(), Success: false}
-			d := createError(e)
+			w.Header().Set("Content-Type", "application/json")
+
+			var b bodyRequest
+			if err := json.NewDecoder(r.Body).Decode(&b); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				e := Error{Data: "Unable to decode JSON request body: " + err.Error(), Success: false}
+				d := createError(e)
+				w.Write(d)
+				return
+			}
+
+			b.URL = strings.TrimSpace(b.URL)
+
+			if b.URL == "" {
+				w.WriteHeader(http.StatusBadRequest)
+				e := Error{Data: "URL is Empty", Success: false}
+				d := createError(e)
+				w.Write(d)
+				return
+			}
+
+			c, err := storage.Save(b.URL)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				e := Error{Data: err.Error(), Success: false}
+				d := createError(e)
+				w.Write(d)
+				return
+			}
+
+			response := Response{Data: prefix + c, Success: true}
+			d := createResponse(response)
 			w.Write(d)
+
+		} else {
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
-
-		b.URL = strings.TrimSpace(b.URL)
-
-		if b.URL == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			e := Error{Data: "URL is Empty", Success: false}
-			d := createError(e)
-			w.Write(d)
-			return
-		}
-
-		c, err := storage.Save(b.URL)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			e := Error{Data: err.Error(), Success: false}
-			d := createError(e)
-			w.Write(d)
-			return
-		}
-
-		response := Response{Data: prefix + c, Success: true}
-		d := createResponse(response)
-		w.Write(d)
 	}
 
 	return http.HandlerFunc(handleFunc)
@@ -55,21 +62,28 @@ func EncodeHandler(storage storages.IFStorage) http.Handler {
 
 func DecodeHandler(storage storages.IFStorage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		code := r.URL.Path[len("/info/"):]
+		if r.Method == http.MethodGet {
+			w.Header().Set("Content-Type", "application/json")
+			code := r.URL.Path[len("/info/"):]
 
-		model, err := storage.LoadInfo(code)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			e := Error{Data: "URL Not Found", Success: false}
-			d := createError(e)
+			model, err := storage.LoadInfo(code)
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				e := Error{Data: "URL Not Found", Success: false}
+				d := createError(e)
+				w.Write(d)
+				return
+			}
+
+			response := Response{Data: model, Success: true}
+			d := createResponse(response)
 			w.Write(d)
+
+		} else {
+
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
-
-		response := Response{Data: model, Success: true}
-		d := createResponse(response)
-		w.Write(d)
 	}
 
 	return http.HandlerFunc(handleFunc)
@@ -77,16 +91,23 @@ func DecodeHandler(storage storages.IFStorage) http.Handler {
 
 func RedirectHandler(storage storages.IFStorage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		code := r.URL.Path[len("/"):]
+		if r.Method == http.MethodGet {
+			code := r.URL.Path[len("/"):]
 
-		model, err := storage.Load(code)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte("URL Not Found"))
+			model, err := storage.Load(code)
+			if err != nil {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte("URL Not Found"))
+				return
+			}
+
+			http.Redirect(w, r, string(model.Url), 301)
+
+		} else {
+
+			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
-
-		http.Redirect(w, r, string(model.Url), 301)
 	}
 
 	return http.HandlerFunc(handleFunc)
