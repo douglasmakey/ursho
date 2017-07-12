c352b77e7433ef2b95bc101a0fd43d5ae74a595e
Douglasmakey <douglasmakey@gmail.com>
2017-07-12 13:25:54 -0400

    Change function ReadConfig, use buffer

--- config/config.go
+++ config/config.go
@@ -3,15 +3,14 @@ package config
 import (
 	"encoding/json"
 	"os"
-	"bufio"
 	"io"
+	"bytes"
 )
 
 type Config struct {
 	Server     Server     `json:"server"`
 	Redis      Redis      `json:"redis"`
 	Postgres   Postgres   `json:"postgres"`
-	Filesystem Filesystem `json:"files_system"`
 	Options    Options    `json:"options"`
 }
 
@@ -33,50 +32,32 @@ type Postgres struct {
 	DB       string `json:"db"`
 }
 
-type Filesystem struct {
-	Path string `json:"path"`
-}
-
 type Options struct {
 	Prefix string `json:"prefix"`
 }
 
 func ReadConfig() (*Config, error) {
 	var objectConfig *Config
+	var buf bytes.Buffer
 
 	// open input file
 	file, err := os.Open("./config/config.json")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	// close file on exit and check for its returned error
-	defer func() {
-		if err := file.Close(); err != nil {
-			panic(err)
-		}
-	}()
+	// close file
+	defer file.Close()
 
-	// make a read buffer
-	r := bufio.NewReader(file)
+	// copy to buffer
+	_, err = io.Copy(&buf, file)
+	if err != nil {
+		return nil, err
+	}
 
-	// make a buffer to keep chunks that are read
-	buf := make([]byte, 1024)
-
-	for {
-		// read a chunk
-		n, err := r.Read(buf)
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-		if n == 0 {
-			break
-		}
-		// Unmarshal data
-		if err := json.Unmarshal(buf[:n], &objectConfig); err != nil {
-			return nil, err
-		}
-
+	// Unmarshal data
+	if err := json.Unmarshal(buf.Bytes(), &objectConfig); err != nil {
+		return nil, err
 	}
 
 	return objectConfig, nil
