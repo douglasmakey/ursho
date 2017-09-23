cfe88cbdcea360ebf884053e1815fd29653847b1
Francesc Campoy Flores <campoy@golang.org>
2017-09-22 18:29:02 -0700

    remove unnecessary channel for graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -34,46 +35,31 @@ func main() {
 	http.Handle("/", handlers.RedirectHandler(storage))
 	http.Handle("/info/", handlers.DecodeHandler(storage))
 
-	// Graceful shutdown
-	sigquit := make(chan os.Signal, 1)
-	signal.Notify(sigquit, os.Interrupt, os.Kill)
-
-	// Wait signal
-	close := make(chan bool, 1)
-
 	// Create a server
 	server := &http.Server{Addr: fmt.Sprintf("%s:%s", config.Server.Host, config.Server.Port)}
 
-	// Start server
-	go func() {
-		log.Printf("Starting HTTP Server. Listening at %q", server.Addr)
-		if err := server.ListenAndServe(); err != nil {
-			if err := server.ListenAndServe(); err != nil {
-				if err != http.ErrServerClosed {
-					log.Println(err.Error())
-				} else {
-					log.Println("Server closed!")
-				}
-				close <- true
-			}
-		}
-
-	}()
-
 	// Check for a closing signal
 	go func() {
+		// Graceful shutdown
+		sigquit := make(chan os.Signal, 1)
+		signal.Notify(sigquit, os.Interrupt, os.Kill)
+
 		sig := <-sigquit
 		log.Printf("caught sig: %+v", sig)
 		log.Printf("Gracefully shutting down server...")
 
-		if err := server.Shutdown(nil); err != nil {
-			log.Println("Unable to shut down server: " + err.Error())
-			close <- true
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Printf("Unable to shut down server: %v", err)
 		} else {
 			log.Println("Server stopped")
-			close <- true
 		}
 	}()
 
-	<-close
+	// Start server
+	log.Printf("Starting HTTP Server. Listening at %q", server.Addr)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("%v", err)
+	} else {
+		log.Println("Server closed!")
+	}
 }
