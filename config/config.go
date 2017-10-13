4d3b075388c6fb8017928de2f6ae1ca132c4c87f
Eri Bastos <ebastos@google.com>
2017-10-13 13:21:27 -0600

    Added support for sqlite3 database backend

--- config/config.go
+++ config/config.go
@@ -22,6 +22,9 @@ type Config struct {
 		Password string `json:"password"`
 		DB       string `json:"db"`
 	} `json:"postgres"`
+	Sqlite struct {
+		FilePath string `json:"filepath"`
+	} `json:"sqlite"`
 	Options struct {
 		Prefix string `json:"prefix"`
 	} `json:"options"`
